9288ad6e2c8860d37a1bc8560f7fddda830a49d4
giftdavid101 <giftdavid101@gmail.com>
2021-03-11 19:07:43 +0100

    homepage-added images, styling

--- src/components/Logobox/index.jsx
+++ src/components/Logobox/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './logobox.style.css'
-import {Link} from 'react-router-dom'
-
+import {AiOutlineHtml5,DiCss3, IoLogoJavascript,IoLogoReact, IoLogoNodejs} from "react-icons/all";
+import {useSpring, animated} from 'react-spring';
 
 const LogoBox = () => {
+
+    const props = useSpring({opacity: 1, from: {opacity: 0}})
+
+
     return (
         <div className={'logo-box'}>
            <header>
@@ -11,14 +15,23 @@ const LogoBox = () => {
            </header>
 
             <div className={'logo-box_intro-text'}>
-                <span>
-                     I am a front-end developer. Let me bring your designs to live.
-                </span>
-                <div className={'logo-box_intro-text_touch'}>
-                   <span className={'logo-box_intro-text_touch_touch-link'}> <Link  to={'/chat'}>Get in touch</Link></span>
+                <div>
+                     <h1>I am a Front-End Developer.</h1>
+                    <animated.span style={props}> Let me bring your designs to live with
+                        modern technologies.</animated.span>
+                </div>
+                <div className={'technologies'}>
+                    <span className={'space'}><AiOutlineHtml5/></span>
+                    <span className={'space'}> <DiCss3/></span>
+                    <span className={'space'}><IoLogoJavascript/></span>
+                    <span className={'space'}> <IoLogoReact/></span>
+                    <span className={'space'}> <IoLogoNodejs/> </span>
+
                 </div>
 
+
             </div>
+
         </div>
     );
 };
--- src/components/Navbox/index.jsx
+++ src/components/Navbox/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './navbox.style.css';
 import NavBar from "../navbar";
+import Code from '../../img/code2.jpg'
 
 const NavBox = () => {
     return (
-        <div className={'navbox'}>
+        <div className={'navbox'} style={{
+            backgroundImage:`url(${Code})`
+        }}>
         <NavBar/>
         </div>
     );
