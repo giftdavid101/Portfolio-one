a4337d4824a1fee31ce267c022b178fde24ec6ce
giftdavid101 <giftdavid101@gmail.com>
2021-03-12 14:11:23 +0100

    added react-typical

--- src/components/Logobox/index.jsx
+++ src/components/Logobox/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './logobox.style.css'
 import {AiOutlineHtml5,DiCss3, IoLogoJavascript,IoLogoReact, IoLogoNodejs} from "react-icons/all";
 import {useSpring, animated} from 'react-spring';
+import Typical from 'react-typical';
 
 const LogoBox = () => {
 
@@ -16,9 +17,14 @@ const LogoBox = () => {
 
             <div className={'logo-box_intro-text'}>
                 <div>
-                     <h1>I am a Front-End Developer.</h1>
-                    <animated.span style={props}> Let me bring your designs to live with
-                        modern technologies.</animated.span>
+                    <animated.span style={props}>  <h1>I am a Front-End Developer.</h1></animated.span>
+                    <Typical
+                        steps={['Hello!', 1000, 'Let me bring your', 500 ,'designs to live with', 500]}
+                        loop={Infinity}
+                        wrapper="p"
+                    />
+
+                        modern technologies.
                 </div>
                 <div className={'technologies'}>
                     <span className={'space'}><AiOutlineHtml5/></span>
